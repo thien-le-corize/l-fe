bd00fcdb5335f715470d4b815a4e17178d94c754
thien-le-corize <thien.le@corize.co.jp>
2024-04-20 23:16:07 +0700

    Reapply "home page"
    This reverts commit c5c5e6ac4c49ab2719c7e149604fa03893ebd720.

--- src/app/pages/Individual/HomeBanners/HomeBanners.tsx
+++ src/app/pages/Individual/HomeBanners/HomeBanners.tsx
@@ -1,22 +1,21 @@
 import { atom, useSetAtom } from "jotai";
 import React, { useCallback, useMemo } from "react";
-import { EffectFade, Parallax } from "swiper/modules";
 import { Swiper } from "swiper/types";
 import { SwiperSlider } from "@/app/components/Swiper";
-import Banner1 from './assets/banner1.png'
-import Banner2 from './assets/banner2.png'
+import Banner1 from "./assets/banner1.png";
+import Banner2 from "./assets/banner2.png";
 import { styled } from "styled-components";
 
 export const homeSwiper = atom<Swiper | null>(null);
 
- const HomeBanners = () => {
+const HomeBanners = () => {
   const setSwiper = useSetAtom(homeSwiper);
 
   const handleSwiper = useCallback(
     (swiper: Swiper) => {
       setSwiper(swiper);
     },
-    [setSwiper],
+    [setSwiper]
   );
 
   const defaultBanners = useMemo(
@@ -24,9 +23,9 @@ export const homeSwiper = atom<Swiper | null>(null);
       { id: 1, url: Banner1.src },
       { id: 2, url: Banner2.src },
     ],
-    [],
-  )
-  
+    []
+  );
+
   const config = { effect: "fade", modules: [EffectFade, Parallax] };
 
   return (
@@ -35,16 +34,13 @@ export const homeSwiper = atom<Swiper | null>(null);
         banners={[...defaultBanners, ...defaultBanners]}
         onSwiper={handleSwiper}
         config={config}
-      >
-      </SwiperSlider>
-
+      ></SwiperSlider>
     </HomeBannersWrapper>
   );
 };
 
-export default HomeBanners
+export default HomeBanners;
 
 const HomeBannersWrapper = styled.div`
   position: relative;
 `;
-
